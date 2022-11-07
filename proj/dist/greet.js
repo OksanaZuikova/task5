7508404ce9f7a02314ffda35524db5648d6259f5
Oksana Zuikova <Oksy82714@gmail.com>
2022-11-07 15:36:01 +0200

    add timer

---
+++ proj/dist/greet.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+function sayHello(name) {
+    return "\u041F\u0440\u0438\u0432\u0435\u0442 \u043E\u0442 ".concat(name);
+}
+exports.sayHello = sayHello;
